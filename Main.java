b0e0cd18ffeda0be73058787b4ff57459a2eacf1
david6698200 <davidcohen8200@gmail.com>
2026-06-30 15:25:51 +0300

    main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[]args){
         System.out.println("Hello World");
         System.out.println("דודו טיפש וחייב לי כסף על ההתערבות");
+        System.out.println(",heui ygu, cxu; vut kt jhhc kh");
     }
 }
